0b310966dc43397390bf94fabb68fd6d07c3591e
JERICHO.Z <547243012@qq.com>
2021-12-28 22:41:10 -0800

    commit UpdateMessageCommand.cs

---
+++ SugarChat.Message/Commands/Messages/UpdateMessageCommand.cs
@@ -0,0 +1,15 @@
+﻿using Mediator.Net.Contracts;
+using SugarChat.Message.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SugarChat.Message.Commands.Messages
+{
+    public class UpdateMessageCommand : ICommand
+    {
+        public IEnumerable<MessageDto> Messages { get; set; }
+    }
+}
