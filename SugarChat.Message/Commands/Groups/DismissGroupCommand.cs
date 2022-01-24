d0d9b66a6dcb614a95057a56d5f53e58a0ba0aa3
JERICHO.Z <547243012@qq.com>
2022-01-23 16:51:19 -0800

    commit

--- SugarChat.Message/Commands/Groups/DismissGroupCommand.cs
+++ SugarChat.Message/Commands/Groups/DismissGroupCommand.cs
@@ -4,7 +4,6 @@ namespace SugarChat.Message.Commands.Groups
 {
     public class DismissGroupCommand : ICommand
     {
-        public string UserId { get; set; }
         public string GroupId { get; set; }
     }
 }
