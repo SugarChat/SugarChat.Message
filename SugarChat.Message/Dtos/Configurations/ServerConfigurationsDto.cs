8718ea958637477b34d27387f2e1fd5e07ab26f3
Brian <BrianCodeman@protonmail.com>
2022-05-10 16:25:02 +0800

    change type of RevokeTimeLimitInMinute to int to represent minutes

--- SugarChat.Message/Dtos/Configurations/ServerConfigurationsDto.cs
+++ SugarChat.Message/Dtos/Configurations/ServerConfigurationsDto.cs
@@ -4,6 +4,6 @@ namespace SugarChat.Message.Dtos.Configurations
 {
     public class ServerConfigurationsDto
     {
-        public TimeSpan RevokeLimitTimeSpan  { get; set; }
+        public int RevokeTimeLimitInMinute  { get; set; }
     }
 }
