45866be5c90e24b33af24ce70f1b5d6fc3bd892e
JERICHO.Z <547243012@qq.com>
2023-09-28 08:22:54 +0000

    批量发送信息

---
+++ SugarChat.Message/Commands/GroupUsers/BatchSetGroupMemberCustomFieldCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SugarChat.Message.Commands.GroupUsers
+{
+    public class BatchSetGroupMemberCustomFieldCommand
+    {
+        public IEnumerable<SetGroupMemberCustomFieldCommand> SetGroupMemberCustomFieldCommands { get; set; }
+    }
+}
